a7b239653c0234daaef0f4cad6921f4c901661bf
VoloUIAutomation <Voloautomation@gmail.com>
2022-06-17 09:44:25 +0400

    delete files

--- TestData/test_data.py
+++
@@ -1,5 +0,0 @@
-# test data
-url ="https://www.roots.com/us/en/homepage"
-email = "group1@mailinator.com"
-password = "pass8888"
-search_text = "shorts"
--- Tests/test_product_sorting.py
+++
@@ -1,9 +0,0 @@
-from pages import sign_in, sort_results
-from helpers import helpers
-from testData import test_data
-
-
-def test_sorting(driver):
-    helpers.go_to_page(driver, test_data.url)
-    sign_in.sing_in_account(driver)
-    sort_results.search_product(driver)
